9a3339c7c7fbcfbaccbd164131f8307891c84a50
Pilinszki-Nagy Csongor <pnagycsongi@gmail.com>
2015-03-22 16:42:15 +0100

    Cell osztály kitöltése
    Még este mókolunk a Gabival karöltve hogy a GameMap és a Cell szinkronban legyenek.

--- szoftlab4Code/src/hu/stapelspeicher/main/Cell.java
+++ szoftlab4Code/src/hu/stapelspeicher/main/Cell.java
@@ -8,16 +8,87 @@ public class Cell {
 	private Position pos;
 	private GameMap map;
 	
-	public void add(ActiveObject ao) {}
-	public void add(Trap t) {}
+	public void add(ActiveObject ao) {
+		Logger.enterFunction("add(ActiveObject ao)", this);
+		
+		actors.add(ao);
+		
+		Logger.exitFunction();
+	}
+	
+	public void add(Trap t) {
+		Logger.enterFunction("add(Trap t)", this);
+		
+		trap = t;
+		
+		Logger.exitFunction();
+	}
+	
 	public Cell getCellFromHere(Position p) {
+		Logger.enterFunction("getCellFromHere(Position p)", this);		
+		
+		int newX = pos.getX() + p.getX();
+		int newY = pos.getY() + p.getY();
+		map.getCell(new Position(newX, newY));
+		
+		Logger.exitFunction();
 		return null;}
-	public void remove(ActiveObject ao) {}
+	
+	public void remove(ActiveObject ao) {
+		Logger.enterFunction("remove(ActiveObject ao)", this);
+		
+		actors.remove(ao);
+		
+		Logger.exitFunction();
+	}
+	
 	public boolean isEmpty() {
-		return false;}
+		Logger.enterFunction("isEmpty()", this);
+		
+		Logger.exitFunction();
+		return actors.isEmpty();
+	}
+	
 	public double getDistanceFromCell(Cell c) {
-		return 0;}
+		Logger.enterFunction("getDistanceFromCell(Cell c)", this);
+		
+		int xDistance = c.pos.getX() - pos.getX();
+		int yDistance = c.pos.getX() - pos-getY();
+		if(xDistance < 0) xDistance *= -1;
+		if(yDistance < 0) yDistance *= -1;
+		
+		Logger.exitFunction();
+		return xDistance + yDistance;
+	}
+	
 	public Cell getFreeNeighbouringCell() {
-		return null;}
+		Logger.enterFunction("getFreeNeighbouringCell()", this);
+		
+		Position newPos = new Position(pos.getX(), pos.getY());
+		newPos.add(new Position(0,1));
+		if(map.getCell(newPos).isEmpty()) {
+			Logger.exitFunction();
+			return map.getCell(newPos);
+		}
+		newPos.add(new Position(1,-1));
+		if(map.getCell(newPos).isEmpty()) {
+			Logger.exitFunction();
+			return map.getCell(newPos);
+		}
+		newPos.add(new Position(-1,-1));
+		if(map.getCell(newPos).isEmpty()) {
+			Logger.exitFunction();
+			return map.getCell(newPos);
+		}
+		newPos.add(new Position(-1,1));
+		if(map.getCell(newPos).isEmpty()) {
+			Logger.exitFunction();
+			return map.getCell(newPos);
+		}
+			
+		
+		Logger.exitFunction();
+		return null;
+	}
 	
 }
