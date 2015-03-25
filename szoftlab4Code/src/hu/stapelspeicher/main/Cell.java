9374eefb1966376951a54f21a29c762f89d05737
Pilinszki-Nagy Csongor <pnagycsongi@gmail.com>
2015-03-25 11:00:59 +0100

    Cella kommentek

--- szoftlab4Code/src/hu/stapelspeicher/main/Cell.java
+++ szoftlab4Code/src/hu/stapelspeicher/main/Cell.java
@@ -3,12 +3,23 @@ package hu.stapelspeicher.main;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A palyat felepito elemeket megvalosito osztaly
+ * @author Csongor
+ * 
+ */
+
 public class Cell {
 	private List<ActiveObject> actors= new ArrayList<ActiveObject>();
 	private Trap trap=null;
 	private Position pos;
 	private GameMap map;
 	
+	
+	/**
+	 * A cella poziciojat allitja be
+	 * @param pos Az uj pozicio
+	 */
 	public void setPosition(Position pos){
 		Logger.enterFunction("setPosition(Position pos)", this);
 		
@@ -17,6 +28,10 @@ public class Cell {
 		Logger.exitFunction();
 	}
 	
+	/**
+	 * A cellahoz tartozo GameMap-et allitja be
+	 * @param gm A jatek GameMap-je
+	 */
 	public void setMap(GameMap gm){
 		Logger.enterFunction("setMap(GameMap gm)", this);
 		
@@ -25,6 +40,12 @@ public class Cell {
 		Logger.exitFunction();
 	}
 	
+	/**
+	 * A cellan egy ujabb mozgo objektumot helyez el
+	 * es a szukseges utkozteteseket elvegzi a mozgo objektumok
+	 * es foltok kozott
+	 * @param ao Az uj mozgo objektum
+	 */
 	public void add(ActiveObject ao) {
 		Logger.enterFunction("add(ActiveObject ao)", this);
 		
@@ -39,6 +60,10 @@ public class Cell {
 		Logger.exitFunction();
 	}
 	
+	/**
+	 * A cellan egy uj foltot helyez el
+	 * @param t Az uj folt
+	 */
 	public void add(Trap t) {
 		Logger.enterFunction("add(Trap t)", this);
 		
@@ -47,6 +72,11 @@ public class Cell {
 		Logger.exitFunction();
 	}
 	
+	/**
+	 * A cellatol megadott tavolsagra levo cellat adja meg
+	 * @param p A megadott tavolsag
+	 * @return Az uj cella
+	 */
 	public Cell getCellFromHere(Position p) {
 		Logger.enterFunction("getCellFromHere(Position p)", this);		
 		
@@ -56,6 +86,10 @@ public class Cell {
 		return c;
 		}
 	
+	/**
+	 * A cellarol eltavolit egy mozgo objektumot
+	 * @param ao Az eltavolitando mozgo objektum
+	 */
 	public void remove(ActiveObject ao) {
 		Logger.enterFunction("remove(ActiveObject ao)", this);
 		
@@ -64,6 +98,10 @@ public class Cell {
 		Logger.exitFunction();
 	}
 	
+	/**
+	 * Teszteli hogy a cellan vannak-e mozgo objektumok
+	 * @return A cella ures (true)
+	 */
 	public boolean isEmpty() {
 		Logger.enterFunction("isEmpty()", this);
 		
@@ -71,6 +109,11 @@ public class Cell {
 		return actors.isEmpty();
 	}
 	
+	/**
+	 * Ket cella kozotti tavolsagot adja meg
+	 * @param c A masodik cella
+	 * @return A ket cella tavolsaga
+	 */
 	public double getDistanceFromCell(Cell c) {
 		Logger.enterFunction("getDistanceFromCell(Cell c)", this);
 		
@@ -83,6 +126,10 @@ public class Cell {
 		return xDistance + yDistance;
 	}
 	
+	/**
+	 * Egy szabad szomszedos cellat ad vissza
+	 * @return A szabad szomszedos cella
+	 */
 	public Cell getFreeNeighbouringCell() {
 		Logger.enterFunction("getFreeNeighbouringCell()", this);
 		Logger.exitFunction();
